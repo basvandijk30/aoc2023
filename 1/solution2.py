e6256a59df00de9c9fe03eb689c6b34e53ed3881
bas <basvandijk30@gmail.com>
2023-12-02 15:01:58 +0100

    test

---
+++ 1/solution2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+with open("input.txt", "r") as f;
+    calibrations = f.readlines()
+
+numstr = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
